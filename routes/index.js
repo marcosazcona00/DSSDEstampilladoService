5b27c3ab8ec3d0ef738dc66416341c91e29373bb
marcosazcona00 <marcosazcona12300@gmail.com>
2021-10-19 16:11:38 -0300

    Armando service estampillado

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
+var jwt = require('jsonwebtoken');
+
 var express = require('express');
 var router = express.Router();
 
-function estampillar(body){}
+ 
+ 
+
+function estampillar(body){
+  var decoded = jwt.decode(body.credenciales);
+  console.log(decoded)
+
+  return body
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,6 +23,13 @@ router.get('/estampillado', function(req, res, next) {
 });
 
 router.post('/estampillado', function(req, res, next) {
+  /* 
+    {
+      "estatuto": PathEstatuto,
+      "numeroExpediente": numExpediente,
+      "credenciales": JWT_TOKEN 
+    }
+  */
   console.log(req.body)
 
   let estampilla = estampillar(req.body) || {}
@@ -25,4 +42,8 @@ module.exports = router;
 
 //Ejemplo 
 //curl -X POST localhost:3000/estampillado -d '{"key": "value"}' -H  "Content-Type: application/json"
+//curl -X POST https://floating-oasis-96990.herokuapp.com/estampillado -d '{"key": "value"}' -H  "Content-Type: application/json"
 
+
+//curl -X POST https://floating-oasis-96990.herokuapp.com/estampillado -d '{"estatuto": "/home/Escritorio/", "numeroExpediente": 1, "credenciales": "JWDSA987-DSAd_AS"}' -H  "Content-Type: application/json"
+//curl -X POST localhost:3000/estampillado -d '{"estatuto": "/home/Escritorio/", "numeroExpediente": 1, "credenciales": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZF9lc3RhbXBpbGxhZG8iOjEsInBhc3N3b3JkIjoiYWVheW9zb3lzYWJhbGVybyIsImlhdCI6MTUxNjIzOTAyMn0.-j_pFuANumQ-QRQrtMAqLeN1OSGyfREvxTzRhgIi5Ag"}' -H  "Content-Type: application/json"
